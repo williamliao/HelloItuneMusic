858ad29746907fdab47847a69a11137648b8cc69
william.liao <william.liao@mitake.com.tw>
2022-01-20 13:15:25 +0800

    add Model

--- HelloItuneMusic/Search/Model/ItemSearchModel.swift
+++ HelloItuneMusic/Search/Model/ItemSearchModel.swift
@@ -8,5 +8,55 @@
 import Foundation
 
 struct ItemSearchModel {
-    
+    let resultCount: Int
+    let results: [Results]
+}
+
+struct Results: Codable {
+    let wrapperType: String
+    let kind: String
+    let artistId: Int
+    let collectionId: Int
+    let trackId: Int
+    let artistName: String
+    let collectionName: String
+    let trackName: String
+    let collectionCensoredName: String
+    let trackCensoredName: String
+    let collectionArtistId: Int
+    let collectionArtistName: String
+    let collectionArtistViewUrl: String
+    let artistViewUrl: String
+    let collectionViewUrl: String
+    let trackViewUrl: String
+    let previewUrl: String
+    let artworkUrl30: String
+    let artworkUrl60: String
+    let artworkUrl100: String
+    let releaseDate: String
+    let collectionExplicitness: String
+    let trackExplicitness: String
+    let discCount: Int
+    let discNumber: Int
+    let trackCount: Int
+    let trackNumber: Int
+    let trackTimeMillis: Int
+    let country: String
+    let currency: String
+    let primaryGenreName: String
+    let isStreamable: Bool
+    let collectionPrice: Double
+    let trackPrice: Double
+    let contentAdvisoryRating: String
+    let feedUrl: String
+    let trackRentalPrice: Double
+    let collectionHdPrice: Double
+    let trackHdPrice: Double
+    let trackHdRentalPrice: Double
+    let artworkUrl600: String
+    let genreIds: [String]
+    let genres: [String]
+    let shortDescription: String
+    let longDescription: String
+    let hasITunesExtras: Bool
 }
