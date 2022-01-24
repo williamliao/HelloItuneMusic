c7d7e6808d63cf9ad6ea60bdd69e32c9c2784bc3
william.liao <william.liao@mitake.com.tw>
2022-01-24 15:43:02 +0800

    update endpoint

--- HelloItuneMusic/Service/EndPoint.swift
+++ HelloItuneMusic/Service/EndPoint.swift
@@ -12,12 +12,62 @@ struct EndPoint {
     let queryItems: [URLQueryItem]
 }
 
+enum ItunesSort: String {
+    case recent
+}
+
+enum ItunesMedia: String {
+    case music
+    case movie
+    case musicVideo
+    case podcast
+    case audiobook
+    case shortFilm
+    case tvShow
+    case ebook
+    case software
+    case all
+}
+
+enum ItunesEntity: String {
+    case music
+    case podcast
+    case movie
+    case musicVideo
+    case audiobook
+    case shortFilm
+    case tvShow
+    case ebook
+    case software
+    case all
+}
+
 extension EndPoint {
     static func search(matching term: String) -> EndPoint {
         return EndPoint(
             path: "/search",
             queryItems: [
                 URLQueryItem(name: "term", value: term),
+            ]
+        )
+    }
+    
+    static func searchAlbum(matching term: String, limit: Int = 50, entity: ItunesEntity = .music, country: String = "tw", lang: String = "zh-tw") -> EndPoint {
+        
+        var limitValue = limit
+        
+        if limitValue >= 200 {
+            limitValue = 200
+        }
+        
+        return EndPoint(
+            path: "/search",
+            queryItems: [
+                URLQueryItem(name: "term", value: term),
+                URLQueryItem(name: "entity", value: entity.rawValue),
+                URLQueryItem(name: "limit", value: "\(limitValue)"),
+                URLQueryItem(name: "entity", value: entity.rawValue),
+                URLQueryItem(name: "country", value: country),
             ]
         )
     }
